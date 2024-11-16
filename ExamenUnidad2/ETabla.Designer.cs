911b3bab56e9f8dd929017abc647c3eba583bb46
CarlosTrev <alu.21130838@correo.itlalaguna.edu.mx>
2024-11-16 12:56:14 -0600

    Se hacen los dos niveles de acceso (empleado y administrador)

--- ExamenUnidad2/ETabla.Designer.cs
+++ ExamenUnidad2/ETabla.Designer.cs
@@ -29,34 +29,46 @@
         private void InitializeComponent()
         {
             dgvEmpleados = new DataGridView();
+            lbleditar = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvEmpleados).BeginInit();
             SuspendLayout();
             // 
             // dgvEmpleados
             // 
             dgvEmpleados.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvEmpleados.Location = new Point(12, 12);
+            dgvEmpleados.Location = new Point(12, 50);
             dgvEmpleados.Name = "dgvEmpleados";
             dgvEmpleados.RowHeadersWidth = 51;
             dgvEmpleados.Size = new Size(597, 321);
             dgvEmpleados.TabIndex = 0;
             dgvEmpleados.CellContentClick += dataGridView1_CellContentClick;
             // 
+            // lbleditar
+            // 
+            lbleditar.AutoSize = true;
+            lbleditar.Location = new Point(12, 9);
+            lbleditar.Name = "lbleditar";
+            lbleditar.Size = new Size(0, 20);
+            lbleditar.TabIndex = 1;
+            // 
             // ETabla
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(635, 356);
+            ClientSize = new Size(691, 398);
+            Controls.Add(lbleditar);
             Controls.Add(dgvEmpleados);
             Name = "ETabla";
             Text = "Empleadotabla";
             Load += Empleadotabla_Load;
             ((System.ComponentModel.ISupportInitialize)dgvEmpleados).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dgvEmpleados;
+        private Label lbleditar;
     }
 }
